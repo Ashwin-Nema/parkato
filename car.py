aa765cefcd92ad09037f3d7927332eb76feff5f9
ash <ashwinnema06@gmail.com>
2021-02-20 10:46:54 +0530

    restructuring

--- car.py
+++
@@ -1,4 +0,0 @@
-class Car:
-    def __init__(self, registration_number, color):
-        self.registration_number = registration_number
-        self.color = color
--- entities/slots.py
+++ entities/slots.py
@@ -1,5 +1,5 @@
 class Slots:
-    def __init__(self, capacity, user_charge = 0):
+    def __init__(self, capacity, user_charge=0):
         self.register = [False for i in range(capacity)]
         self.displace = []
         self.slot = 0
@@ -10,29 +10,28 @@ class Slots:
     def add(self):
         if all(self.register):
             return False
-        
+
         self.income += self.user_charge
         self.count += 1
-        
+
         if self.displace:
             position = self.displace.pop(0)
-            self.register[position - 1 ] = True
+            self.register[position - 1] = True
             return position
-        
+
         else:
             self.register[self.slot] = True
             self.slot += 1
             return self.slot
 
-    
     def remove(self, position):
         if position > len(self.register):
             return position
-        
-        if self.register[position - 1] == True:
+
+        if self.register[position - 1] is True:
             self.register[position - 1] = False
             self.displace.append(position)
-            
+
             return position
 
         else:
